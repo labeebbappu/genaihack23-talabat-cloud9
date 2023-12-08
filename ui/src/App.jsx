c9c3af5f40d37709d322a38bc62cd3746dff5800
labeebbappu <labeeb.in@outlook.com>
2023-12-08 17:23:16 +0300

    ui initial files updated

---
+++ ui/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Talabat review mining</h1>
+      <p className="text-xl">by Cloud9 on genaihack23</p>
+    </>
+  );
+}
+
+export default App;
