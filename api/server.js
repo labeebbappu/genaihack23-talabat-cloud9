e53ad2adc4ff51bab922821023be5c8622bf611f
labeebbappu <labeeb.in@outlook.com>
2023-12-08 17:50:30 +0300

    api initials added

---
+++ api/server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+
+const port = process.env.PORT || process.env.HTTP_SERVER_PORT || 3000;
+
+const app = express();
+
+ // health check endpoint
+ app.all('/health', (req, res) => {
+    res.json({ health: 'OK' });
+    res.end();
+  });
+
+   // this is not a public domain
+   app.all('/', (req, res) => {
+    res.json({ permission: 'This is not a public domain' });
+    res.end();
+  });
+
+  app.listen(port, () => {
+    console.log(`🚀 http endpoint ready at http://localhost:${port}/`);
+  });
+
+console.log('hello');
